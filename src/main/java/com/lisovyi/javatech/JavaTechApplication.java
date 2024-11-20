fde78a6662297d7d0be7aa76a2f43dafb71d5df8
victorLisovyi <victor.lissoviy.93@gmail.com>
2024-11-20 20:17:40 +0200

    init commit

---
+++ src/main/java/com/lisovyi/javatech/JavaTechApplication.java
@@ -0,0 +1,13 @@
+package com.lisovyi.javatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTechApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaTechApplication.class, args);
+  }
+
+}
